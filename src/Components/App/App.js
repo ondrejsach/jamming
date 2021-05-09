19973e23782c517bc6ddc934637f6410ba1f56fc
Ondrej Sach <ondrejsach@gmail.com>
2021-05-09 19:43:46 +0200

    Part 8 completed

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,7 @@ class App extends React.Component{
         this.addTrack = this.addTrack.bind(this);
         this.removeTrack = this.removeTrack.bind(this);
         this.updatePlaylistName = this.updatePlaylistName.bind(this);
+        this.savePlaylist = this.savePlaylist.bind(this);
     }
 
     addTrack(track){
@@ -45,6 +46,10 @@ class App extends React.Component{
         this.setState({playlistName: name})
     }
 
+    savePlaylist(){
+        const trackURIs = this.state.playlistTracks.map(track => track.uri);
+    }
+
     render(){
         return(
             <div>
@@ -57,7 +62,8 @@ class App extends React.Component{
                   <Playlist playlistName={this.state.playlistName}
                             playlistTracks={this.state.playlistTracks}
                             onRemove={this.removeTrack}
-                            onNameChange={this.updatePlaylistName} />
+                            onNameChange={this.updatePlaylistName}
+                            onSave={this.savePlaylist} />
                 </div>
               </div>
             </div>
